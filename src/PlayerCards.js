d0a8504d56fed2cd950f60d697c1cde6d1acfcd7
Niño Pabellano <ninopabellano@gmail.com>
2026-01-21 09:18:56 +0800

    Update PlayerCards.js

--- src/PlayerCards.js
+++ src/PlayerCards.js
@@ -5,25 +5,13 @@ class PlayerCards extends React.Component {
     const { cards } = this.props;
 
     return (
-      <div style={{ display: "flex", position: "relative", height: "140px" }}>
-        {cards.map((card, index) => (
-          <img
-            key={card.code}
-            src={card.image} // use the card image from API
-            alt={`${card.value} of ${card.suit}`}
-            style={{
-              position: "relative",
-              left: index * -40, // overlap the cards
-              width: "80px",
-              height: "120px",
-              borderRadius: "6px",
-              border: "1px solid #ccc",
-              boxShadow: "1px 1px 3px rgba(0,0,0,0.3)",
-              userSelect: "none",
-            }}
-          />
+      <ul>
+        {cards.map((card) => (
+          <li key={card.code}>
+            <img src={card.image} alt={`${card.value} of ${card.suit}`} />
+          </li>
         ))}
-      </div>
+      </ul>
     );
   }
 }
